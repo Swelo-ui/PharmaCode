e3a086bff154755ff47e928aa5c28d5ec03817b7
Himanshu Sharma <himanshusharma.shriram@gmail.com>
2026-09-06 00:56:02 +0530

    fix(lint): remove unused imports in login and signup screens, add --no-fatal-infos to analyze

--- pharmacode_app/lib/screens/auth/signup_screen.dart
+++ pharmacode_app/lib/screens/auth/signup_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../../core/in_app_browser.dart';
 import '../../core/theme.dart';
 import '../../features/auth/domain/auth_state.dart';
 import '../../features/auth/presentation/auth_controller.dart';
